4c883c9bd7d1d2be2f3b1963127a18ef664723d1
SEB_BE_42_김종규 <whdrb3353@naver.com>
2024-10-02 14:01:39 +0900

    브랜치 바뀐듯 푸시 해볼게요

--- src/scope/Scope1.java
+++ src/scope/Scope1.java
@@ -8,6 +8,7 @@ public class Scope1 {
             int x = 20; // x 생존시작
             System.out.println("if m = " + m);
             System.out.println("if x = " + x);
-        }
-    }
+        } // x 생존 종료
+        System.out.println("main x = " + m);
+    }  // m 생존 종료
 }
---
+++ src/scope/Scope2.java
@@ -0,0 +1,16 @@
+package scope;
+
+public class Scope2 {
+    public static void main(String[] args) {
+        int m = 10;
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("for m = " + m); // 블록 내부에서 외부는 접근 가능
+            System.out.println("for i = " + i);
+        } // i 생존 종료
+
+        System.out.println("main m = " + m);
+        //System.out.println("main i = " + i); i는 for문 내에서 끝나기 때문에 안됌
+
+    }
+}
